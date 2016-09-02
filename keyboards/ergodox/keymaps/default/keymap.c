0f5cf2879dabcd53bde1432adcac6398ad8dfaf4
chooblarin <choo.bla.rin@gmail.com>
2016-09-02 23:18:32 +0900

    Move Equal, Escape, and Back quote

--- keyboards/ergodox/keymaps/default/keymap.c
+++ keyboards/ergodox/keymaps/default/keymap.c
@@ -11,13 +11,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |   [  |           |  ]   |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |   [  |           |  ]   |   6  |   7  |   8  |   9  |   0  | ESC    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  -   |           |  '"  |   Y  |   U  |   I  |   O  |   P  | BS     |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter  |
- * |--------+------+------+------+------+------| Hyper|           |   \  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | /    | RShift |
+ * | Ctrl   |A / L2|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter  |
+ * |--------+------+------+------+------+------|   =  |           |   \  |------+------+------+------+------+--------|
+ * | LShift |Z / L1|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | /    | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  | Alt  | Cmd  |AltShf|                                       | Left | Right| Down |  Up  | Meh  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -33,16 +33,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LBRC,
+        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LBRC,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_MINS,
-        KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LCTRL,       LT(MDIA,KC_A),KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        LT(SYMB,KC_Z),KC_X,   KC_C,   KC_V,   KC_B,   KC_EQL,
         LT(SYMB,KC_GRV),KC_QUOT,      KC_LALT,KC_LGUI,LALT(KC_LSFT),
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_FN1, KC_END,
         // right hand
-             KC_RBRC,     KC_6,   KC_7,   KC_8,   KC_9,    KC_0,            KC_MINS,
+             KC_RBRC,     KC_6,   KC_7,   KC_8,   KC_9,    KC_0,            KC_ESC,
              KC_QUOT,     KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,            KC_BSPC,
                           KC_H,   KC_J,   KC_K,   KC_L,    LT(MDIA,KC_SCLN),KC_ENT,
 		     KC_BSLS,     KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH,         KC_RSFT,
