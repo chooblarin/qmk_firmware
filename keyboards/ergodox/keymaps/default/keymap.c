5b4ee078dd6343374a7749f5654ec762fc1f777b
chooblarin <choo.bla.rin@gmail.com>
2016-09-29 20:25:23 +0900

    Move Left, Right, Down, Up

--- keyboards/ergodox/keymaps/default/keymap.c
+++ keyboards/ergodox/keymaps/default/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   =  |           |   \  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | /    | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|   -  | Alt  | Cmd  |      |                                       | Left | Right| Down |  Up  | Meh  |
+ *   |Grv/L1|   -  | Alt  | Cmd  |      |                                       |  [   |   ]  |   -  |  =   | Meh  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |   [  | LGui |       | ESC  |  ]   |
+ *                                        | Left | Right|       | Down |  Up  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space| ~L1  |------|       |------|  ~L1   | Space|
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL,       KC_A,       KC_S,    KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,       KC_X,    KC_C,   KC_V,   KC_B,   KC_EQL,
         LT(SYMB,KC_GRV),KC_MINS,    KC_LALT, KC_LGUI,KC_TRNS,
-                                               KC_LBRC,  KC_LGUI,
+                                               KC_LEFT,       KC_RGHT,
                                                               KC_HOME,
                                                KC_SPC,KC_FN1, KC_END,
         // right hand
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_QUOT,     KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,            KC_BSPC,
                           KC_H,   KC_J,   KC_K,   KC_L,    LT(MDIA,KC_SCLN),KC_ENT,
 		     KC_BSLS,     KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH,         KC_RSFT,
-                                  KC_LEFT,KC_RGHT,KC_DOWN, KC_UP,           MEH_T(KC_NO),
-             KC_ESC,         KC_RBRC,
+                                  KC_LBRC,KC_RBRC,KC_MINS, KC_EQL,          MEH_T(KC_NO),
+             KC_DOWN,        KC_UP,
              KC_PGUP,
              KC_PGDN,KC_FN1, KC_SPC
     ),
