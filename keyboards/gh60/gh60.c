dccbad6cd414c66864a05fcc83b1145446280a34
ariasuni <perso@hack-libre.org>
2017-05-27 00:25:35 +0200

    Fix undefined reference errors with avr-gcc 7.1

--- keyboards/gh60/gh60.c
+++ keyboards/gh60/gh60.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "gh60.h"
 
 void led_set_kb(uint8_t usb_led) {
@@ -23,3 +24,44 @@ void led_set_kb(uint8_t usb_led) {
 
 	led_set_user(usb_led);	
 }
+=======
+#include "gh60.h"
+
+
+extern inline void gh60_caps_led_on(void);
+extern inline void gh60_poker_leds_on(void);
+extern inline void gh60_fn_led_on(void);
+extern inline void gh60_esc_led_on(void);
+extern inline void gh60_wasd_leds_on(void);
+
+extern inline void gh60_caps_led_off(void);
+extern inline void gh60_poker_leds_off(void);
+extern inline void gh60_fn_led_off(void);
+extern inline void gh60_esc_led_off(void);
+extern inline void gh60_wasd_leds_off(void);
+
+
+void led_set_kb(uint8_t usb_led) {
+	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+		gh60_caps_led_on();
+		} else {
+		gh60_caps_led_off();
+    }
+
+    // if (usb_led & (1<<USB_LED_NUM_LOCK)) {
+		// gh60_esc_led_on();
+		// } else {
+		// gh60_esc_led_off();
+    // }
+
+    // if (usb_led & (1<<USB_LED_SCROLL_LOCK)) {
+		// gh60_fn_led_on();
+		// } else {
+		// gh60_fn_led_off();
+    // }
+
+	led_set_user(usb_led);
+}
+>>>>>>> e87691fa5... Fix undefined reference errors with avr-gcc 7.1
