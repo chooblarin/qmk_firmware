28c34ec8d2be58e5dacd97a2411bb9c49cf1e47a
Sota Hatakeyama <choo.bla.rin@gmail.com>
2018-04-20 13:52:19 +0900

    Move Left, Right, Down, Up

--- keyboards/ergodox/keymaps/default/keymap.c
+++ keyboards/ergodox/keymaps/default/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   =  |           |   \  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | /    | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|   -  | Alt  | Cmd  |      |                                       |   -  |   =  |   [  |  ]   | Meh  |
+ *   |Grv/L1|   -  | Alt  | Cmd  |      |                                       | Left | Right| Down |  Up  | Meh  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Left | Right|       | Down |  Up  |
+ *                                        | Left | Right|       |  (   |   )  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space| ~L1  |------|       |------|  ~L1   | Space|
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_QUOT,     KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,            KC_BSPC,
                           KC_H,   KC_J,   KC_K,   KC_L,    LT(MDIA,KC_SCLN),KC_ENT,
 		     KC_BSLS,     KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH,         KC_RSFT,
-                                  KC_MINS,KC_EQL, KC_LBRC, KC_RBRC,         MEH_T(KC_NO),
-             KC_DOWN,        KC_UP,
+                                  KC_LEFT,KC_RGHT,KC_DOWN, KC_UP,           MEH_T(KC_NO),
+             KC_LPRN,       KC_RPRN,
              KC_PGUP,
              KC_PGDN,KC_FN1, KC_SPC
     ),
